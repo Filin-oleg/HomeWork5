b7dfaef4fdc2b4d8eda751f9b410056b3945c99e
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-08-19 22:10:30 +0200

    done

---
+++ Task002/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях
+
+Console.Clear();
+int[] FillArray(int arrLenght, int minValue, int maxValue)
+{
+    int[] array = new int[arrLenght];
+    for (int i =0; i < arrLenght; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    Console.WriteLine (string.Join (", ", array));
+    return  array;
+}
+int sumOdd (int[] array)
+{
+    int sum = 0;
+    int[] arr = array;
+    for (int i = 1; i < arr.Length; i++, i++)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+int[] array = FillArray(10, 1, 100);
+int result = sumOdd(array);
+Console.WriteLine("Сумма элементов массива, стоящих на нечетных позициях = " + result);
