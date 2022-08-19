3c55e253c98593ff8a4759dfa268098191ccf272
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-08-19 21:48:44 +0200

    done

---
+++ Task001/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.Clear();
+int[] FillArray(int arrLenght)
+{
+    int[] array = new int[arrLenght];
+    for (int i =0; i < arrLenght; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    Console.WriteLine (string.Join (", ", array));
+    return  array;
+}
+int countEven (int [] array)
+{
+    int count = 0;
+    int[] arr = array;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+int[] array = FillArray(20);
+int result = countEven(array);
+Console.WriteLine("Количество четных чисел в данном массиве = " + result);
