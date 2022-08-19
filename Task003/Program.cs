47dc5104316e87c8594dcbf944f57af7c48e8e5c
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-08-19 22:41:16 +0200

    done

---
+++ Task003/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементами массива.
+
+Console.Clear();
+double[] FillArray(int arrLenght, int minValue, int maxValue)
+{
+    double[] array = new double[arrLenght];
+    Random rand = new Random();
+    for (int i =0; i < arrLenght; i++)
+    {
+        array[i] = rand.Next(minValue, maxValue) + rand.NextDouble();
+    }
+    Console.WriteLine (string.Join (", ", array));
+    return  array;
+}
+double max (double[] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+double min (double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+double[] array = FillArray(5, 1, 10);
+double maxx = max (array);
+double minn = min (array);
+double result = maxx - minn;
+Console.WriteLine("Разница между максимальным и минимальным элементами массива = " + result);
